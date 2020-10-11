85f7ca2d4e3dd8848bf46d3cf78252f6c6f5c133
Huawen Yu <hyu@fortinet.com>
2020-10-11 00:03:37 -0700

    improve cmdssh with expect

--- program-lang/python/script/README.md
+++ program-lang/python/script/README.md
@@ -1,10 +1,58 @@
-$ Requirement
+# Requirement
 
 	https://pysvn.sourceforge.io/downloads.html
 
 	$ sudo apt-get install python3-apt
 	$ sudo apt-get install -y python3-pysvn
 
-$ Sample svn-server:
+# Sample svn-server:
 
 	$ svn ls https://svn.alfresco.com/repos/alfresco-open-mirror/integrations/GoogleDocs/HEAD/Google\ Docs\ Share/src/test/resources/alfresco/web-extensio
+
+# Module: cmd
+
+https://pymotw.com/2/cmd/
+
+## Parse arg ourself
+
+https://cmd2.readthedocs.io/en/latest/features/argument_processing.html
+https://github.com/python-cmd2/cmd2/blob/master/examples/example.py#L24
+
+```python
+import cmd
+import urlparse
+
+class myCmd(cmd.Cmd):
+    def onecmd(self, line):
+        """Mostly ripped from Python's cmd.py"""
+        cmd, arg, line = self.parseline(line)
+        arg = urlparse.parse_qs(arg) # <- added line
+        if not line:
+            return self.emptyline()
+        if cmd is None:
+            return self.default(line)
+        self.lastcmd = line
+        if cmd == '':
+            return self.default(line)
+        else:
+            try:
+                func = getattr(self, 'do_' + cmd)
+            except AttributeError:
+                return self.default(line)
+            return func(arg)
+
+    def do_foo(self, arg)
+        print arg
+
+my_cmd = myCmd()
+my_cmd.cmdloop()
+```
+
+## group cmd, enable/disable them
+
+https://cmd2.readthedocs.io/en/latest/features/help.html#categorizing-commands
+https://cmd2.readthedocs.io/en/latest/features/disable_commands.html
+
+# script: cmdssh.py
+
+
--- program-lang/python/script/cmdssh.py
+++ program-lang/python/script/cmdssh.py
@@ -7,6 +7,7 @@ __date__ = "Sep 13, 2020"
 import re
 import cmd2
 import argparse
+from cmdssh.dut.dut import Dut
 
 # https://stackoverflow.com/questions/279237/import-a-module-from-a-relative-path
 import os, sys, inspect
@@ -16,30 +17,50 @@ if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
 
+from cmdssh.dut.dut import Dut
 from base.common import Common, BaseCommon
-from cmdssh.interact import Interact
 
 class CmdSSH:
 
     def __init__(self, args):
         """ctor."""
-        self.common = BaseCommon(self, args)
+        self.args = args
+
+
+    @staticmethod
+    def main(argv=sys.argv[1:]):
+        # Detail explain:
+        #  https://docs.python.org/dev/library/argparse.html
+        prog = os.path.basename(__file__)
+        desc = f"Python script '{prog}':\n"\
+                " {prog} A sample..."
+
+        usage = f"{prog} [options]:\n" \
+                " {prog} -d 10.1.1.2 -u 'admin' -p '' \n" \
+                " {prog} -d 10.1.1.2 -u 'admin' -p '' -t gdb:wad \n" \
+                " {prog} -d 10.1.1.2 -u 'admin' -p '' -t log:wad,ips,urlfilter;show:wad \n"
+
+        parser = argparse.ArgumentParser(description=desc, prog=prog, usage=usage)
+        parser.add_argument("-e", "--command", help="command execute")
+        parser.add_argument("-t", "--task", help="execute exist tasks")
+        parser.add_argument("-n", "--dryrun", action="store_false",
+                help="simulate dryrun, don't really send the command to target")
+        parser.add_argument("-d", "--host", help="The target domain name or IP address", required=True)
+        parser.add_argument("-u", "--username", help="The username used logon the target", required=True)
+        parser.add_argument("-p", "--password", help="The password used logon the target")
+        BaseCommon.add_arg_verbose(parser)
+
+        args = parser.parse_args(argv)
+        sys.argv = [sys.argv[0]]    # Clear the argv
+        cmd1 = CmdSSH(args)
+        cmd1.run()
 
 
     def run(self):
-        act = Interact(self.common)
+        act = Dut(self.args)
         act.run()
 
 
 if __name__ == "__main__":
-
-    desc = f"Python script '{os.path.basename(__file__)}': A sample ..."
-    print(desc)
-    parser = argparse.ArgumentParser(description=desc)
-    BaseCommon.add_arg_verbose(parser)
-
-    args = parser.parse_args()
-    cmd1 = CmdSSH(args)
-    cmd1.run()
-
+    cmd1 = CmdSSH.main()
 
